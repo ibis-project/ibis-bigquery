8047fe611ecdcecfc15792a90c2f79005cf28d93
Tim Swast <swast@google.com>
2021-04-23 10:09:56 -0500

    install in editable mode so that entrypoints are available

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 python -m pip install --upgrade pip
-python -m pip install ./ibis
-python -m pip install .
+python -m pip install -e ./ibis
+python -m pip install -e .
 python -m pip install pytest
