e5975424e018abcd8fe6381d643e131b45114f44
Tim Swast <swast@google.com>
2021-04-26 16:37:02 -0500

    fix indentation

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -7,10 +7,11 @@ python -m pip install --upgrade pip
 
 if [ "$IBIS_VERSION" = "github" ] ; then
     # See https://github.com/pypa/pip/issues/7953
-    echo "import site
-    import sys
-    site.ENABLE_USER_SITE = '--user' in sys.argv[1:]
-    $(cat ./ibis/setup.py)" > ./ibis/setup.py
+    echo "
+import site
+import sys
+site.ENABLE_USER_SITE = '--user' in sys.argv[1:]
+$(cat ./ibis/setup.py)" > ./ibis/setup.py
 
     python -m pip install --user -e ./ibis
 else
