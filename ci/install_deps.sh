acebfe21c8eca051ec854383c701d9b0c1644aff
Tim Swast <swast@google.com>
2021-04-26 10:18:49 -0500

    try installing to HOME for GitHub Actions

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 python -m pip install --upgrade pip
-python -m pip install -e ./ibis
-python -m pip install -e .
-python -m pip install pytest
+python -m pip install --user -e ./ibis
+python -m pip install --user -e .
+python -m pip install --user pytest
