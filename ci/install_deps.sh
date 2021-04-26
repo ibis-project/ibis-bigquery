f8c3e344357b8f98366dfb6729955eed83339ea0
Tim Swast <swast@google.com>
2021-04-26 16:25:13 -0500

    ci: run tests with multiple versions of ibis

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -3,13 +3,19 @@
 set -e
 set -x
 
-# See https://github.com/pypa/pip/issues/7953
-echo "import site
-import sys
-site.ENABLE_USER_SITE = '--user' in sys.argv[1:]
-$(cat ./ibis/setup.py)" > ./ibis/setup.py
-
 python -m pip install --upgrade pip
-python -m pip install --user -e ./ibis
+
+if [ "$IBIS_VERSION" = "github" ] ; then
+    # See https://github.com/pypa/pip/issues/7953
+    echo "import site
+    import sys
+    site.ENABLE_USER_SITE = '--user' in sys.argv[1:]
+    $(cat ./ibis/setup.py)" > ./ibis/setup.py
+
+    python -m pip install --user -e ./ibis
+else
+    python -m pip install --user ibis-framework=="$IBIS_VERSION"
+fi
+
 python -m pip install --user -e .
 python -m pip install --user pytest
