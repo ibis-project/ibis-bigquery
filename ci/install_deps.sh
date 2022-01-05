e9c953535fbe6c83f154aca9204f6dc983c19a0e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-05 10:30:16 -0500

    ci: fixes to address upcoming ibis incompatibilities (#106)
    * test: remove noop constructor
    
    * fix: ensure non-nested installation
    
    * ci: fixup deps
    
    * ci: remove explicit head sha as pull_request_target already runs without the merge commit

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -13,10 +13,10 @@ import sys
 site.ENABLE_USER_SITE = '--user' in sys.argv[1:]
 $(cat ./ibis/setup.py)" > ./ibis/setup.py
 
-    python -m pip install --user -e ./ibis
+    python -m pip install --user ./ibis
 else
     python -m pip install --user ibis-framework=="$IBIS_VERSION"
 fi
 
-python -m pip install --user -e .
+python -m pip install --user ./ibis_bigquery
 python -m pip install --user pytest
