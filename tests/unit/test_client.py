dc474af94bb8590c9acf3ec3f94634f366349580
Hussain Sultan <hussainz@gmail.com>
2022-03-15 11:59:18 -0400

    feat: Raise better error message when incorrect dataset is supplied #113 (#115)
    * feat: Raise better error message on dataset [#113]
    
    * chore: refactor tests to use pytests.raises matcher
    
    Co-authored-by: Tim Swast <swast@google.com>

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -19,3 +19,11 @@ import ibis_bigquery.client
 def test_parse_project_and_dataset(project, dataset, expected):
     got = ibis_bigquery.client.parse_project_and_dataset(project, dataset)
     assert got == expected
+
+
+def test_parse_project_and_dataset_raises_error():
+    expected_message = "data-project.my_dataset.table is not a BigQuery dataset"
+    with pytest.raises(ValueError, match=expected_message):
+        ibis_bigquery.client.parse_project_and_dataset(
+            "my-project", "data-project.my_dataset.table"
+        )
