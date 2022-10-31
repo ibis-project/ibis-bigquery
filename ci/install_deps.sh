ca7d56d4a1e7dd3506ea1493dad58ec47c85eeaa
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-10-31 13:54:37 -0400

    ci: use poetry 1.2

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -7,7 +7,7 @@ python -m pip install --upgrade pip
 if [ "$IBIS_VERSION" = "github" ] ; then
     # these deps are to ensure that unreleased versions of upstream ibis are
     # reflected in its version
-    python -m pip install --upgrade 'poetry<1.2' 'poetry-dynamic-versioning<0.18.0'
+    python -m pip install --upgrade 'poetry>=1.2' 'poetry-dynamic-versioning>=0.18.0'
     python -m pip install 'git+https://github.com/ibis-project/ibis.git@master'
     python -c 'import ibis; print(ibis.__version__)'
 else
